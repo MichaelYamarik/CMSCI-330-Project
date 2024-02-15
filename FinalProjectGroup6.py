2ce50331dfb30b693a4fcff868c99a6269774c31
MichaelYamarik <153024883+MichaelYamarik@users.noreply.github.com>
2024-02-15 10:41:23 -0500

    Update FinalProjectGroup6.py

--- FinalProjectGroup6.py
+++ FinalProjectGroup6.py
@@ -1,1 +1,28 @@
-#TEst for pulling workie
+import numpy as np
+
+#Functions
+
+# # Function for assigning a GPA to a number
+
+# # # Probably use an array for the numbers and their assigned letter to save space
+
+
+# # Function for reading in a section file and calling the GPA Function
+
+
+
+#Read in RUN file
+
+# # For loop to read all group files
+
+# # Read in first GRP file
+
+# # # For loop to read in each section in the group file and call functions before next GRP
+
+# # # Assign Section GPAS to a dataframe with columns, SECTION NAME, GPA NUMBER
+
+#For each section file, call the 
+
+#Remove Commas, replace with space or
+#Put into 2d Numpy Array, while removing commas
+

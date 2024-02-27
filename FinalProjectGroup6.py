1b0c22a4f4090340444c2dfb82ee3a8b39190568
Michael Yamarik <myamarik591@g.rwu.edu>
2024-02-27 13:14:24 -0500

    Created methods for assigning RUNS to GRPS, and GRPS to SECs.
    Temporary Methods that probably will be subject to
    change
    
    Also created a method for assigning letters to
    grades.

--- FinalProjectGroup6.py
+++ FinalProjectGroup6.py
@@ -1,28 +1,55 @@
 import numpy as np
+import pandas as pd
 
-#Functions
+# Functions
 
-# # Function for assigning a GPA to a number
+# # Creating 2D arrays from a group, 2D is for the sections
 
-# # # Probably use an array for the numbers and their assigned letter to save space
+def create_GRP(run):   
+    data = [run]  
+    with open(run) as file:
+        line = file.readlines()  
+        for x in line:
+            x = x.strip()
+            if x.endswith('.GRP'):
+                data.append(x)
+    return np.array(data)
 
 
-# # Function for reading in a section file and calling the GPA Function
+def create_SEC(grp):
+    data = [grp]
+    with open(grp) as file:
+        line = file.readlines()
+        for x in line:
+            x = x.strip()
+            if x.endswith('.SEC'):
+                data.append(x)
+    return np.array(data)
 
 
+def letter_to_number(letterGrade):
+    gpaLetter = {
+        'A': 4.0,
+        'A-': 3.7,
+        'B+': 3.3,
+        'B': 3.0,
+        'B-': 2.7,
+        'C+': 2.3,
+        'C': 2.0,
+        'C-': 1.7,
+        'D+': 1.3,
+        'D': 1.0,
+        'F': 0.0,
+        'I': None,  
+        'W': None,  
+        'P': None,  
+        'NP': None
+    }
+    
+    return gpaLetter.get(letterGrade,None)
 
-#Read in RUN file
+grp_arr = create_GRP("TESTRUN.RUN")
+sec_arr = create_SEC(grp_arr[1])
 
-# # For loop to read all group files
-
-# # Read in first GRP file
-
-# # # For loop to read in each section in the group file and call functions before next GRP
-
-# # # Assign Section GPAS to a dataframe with columns, SECTION NAME, GPA NUMBER
-
-#For each section file, call the 
-
-#Remove Commas, replace with space or
-#Put into 2d Numpy Array, while removing commas
-
+print(grp_arr)
+print(sec_arr)

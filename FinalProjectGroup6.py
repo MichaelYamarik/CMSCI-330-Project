6081d72939369a864667dfcd77030a7753fd3c85
Michael Yamarik <myamarik591@g.rwu.edu>
2024-04-10 16:39:00 -0400

    Everything should be done now

--- FinalProjectGroup6.py
+++ FinalProjectGroup6.py
@@ -1,12 +1,18 @@
 import numpy as np
 import pandas as pd
+from collections import Counter
+import tkinter as tk
+from tkinter import filedialog
+import os
 # Functions
 
 #globals
 
 temp_sec = []
 
+temp_students = []
 
+temp_bad_students = []
 
 # # Creating 2D arrays from a group, 2D is for the sections
 
@@ -118,6 +124,7 @@ def letter_to_number(letterGrade):
         'P': None,  
         'NP': None
     }
+    
     #returns a get function to see what the inputted letter is related to for GPA's
     return gpaLetter.get(letterGrade,None)
 
@@ -143,6 +150,17 @@ def calc_SEC_GPA(array):
     summy = summy * credit
     #prints the GPA of the section
     
+    for x in data:
+        for y in x:
+            temp = y[2]
+            if temp is not None:
+                if temp >= 3.7:
+                    temp_students.append(y[0])
+                elif temp <= 1.3:
+                    temp_bad_students.append(y[0])
+    
+    
+    
     newGPA = (np.sum(summy))/(credit*np.size(summy))
     
     temp_sec.append(newGPA)
@@ -156,6 +174,15 @@ def empty_list(array):
     global temp_sec
     temp_sec.clear()
 
+
+
+def remove_non_duplicates(array):
+    name_counts = Counter(student[0] for student in array)
+    duplicates = [name for name, count in name_counts.items() if count > 1]
+    filtered_array = [student for student in array if student[0] in duplicates]
+    return filtered_array
+
+
 #########    NOT FINAL    ###################
 def begin(run):
     
@@ -164,6 +191,10 @@ def begin(run):
     empty_list(temp_sec)
     #creates an array of the groups 
     array = create_GRP(run)
+    
+    update_text("Getting the Groups...")
+    
+    update_text("Calculating the SEC GPA's...")
     
     for x in array[1:]:
         
@@ -192,7 +223,7 @@ def begin(run):
             std_dev = np.std(temp_sec)
             #print(f"Section {y}'s GPA is {sectempgpa}")
             #print(f"Current Group, {x}'s GPA is {temp_sum }")
-        file.write(f"\nThe GPA of group {x} is: {temp_sum}\n\n")
+        file.write(f"\nThe GPA of group {x} is: {round(temp_sum,3)}\n\n")
         newtemp = create_SEC(x)
         newcount = 0
         for g in newtemp[1:]:
@@ -201,10 +232,70 @@ def begin(run):
             newsectemp = sec_gps[newcount]
             newcount = newcount + 1
             z_score = (newsectemp - temp_sum) / std_dev
-            file.write(f"Section {g}'s GPA is {newsectemp} and the Z-Score is {z_score}\n")       
+            file.write(f"\nSection {g}'s GPA is {round(newsectemp,3)}") 
+            if z_score >= 2 or z_score <= -2:
+                file.write(f" and the relevant Z-Score is {round(z_score,2)}")
+            else:
+                file.write(".")
 
         file.write("\n\n")
+    
+    update_text("Getting Relevant Students...")
+    
+    important_Students = remove_non_duplicates(temp_students)
+    help_students = remove_non_duplicates(temp_bad_students)
+    
+    update_text("Printing...")
+    
+    file.write(f"\nHere are the students with more than one A-, A\n")
+    for x in important_Students:
+        file.write(f"{x}\n")
+        
+    file.write(f"\nHere are the students with more than one D+, D, D-, F\n")
+    for x in help_students:
+        file.write(f"{x}\n")
+        
+    update_text("DONE! Please go to the file location of the .RUN file for the .txt file!")
+    
+    
+
+window = tk.Tk()
+window.title("GPA Project Calculator")
+window.geometry("700x350")
+
+run_file = ""
+
+def upload_file():
+    file_path = filedialog.askopenfilename(filetypes=[("Runnable Files", "*.run")])
+    print("Selected file:", file_path)
+    run_file = file_path
+    begin(run_file)
+
+
+def go_to_text():
+    file_name = "GPA_Results.txt"
+    try:
+        notepad_command = f"notepad {file_name}"
+        os.system(notepad_command)
+    except FileNotFoundError:
+        update_text("Error:File not found! Contact Distributor.")
+    
+
+upload_button = tk.Button(window, text="Upload .run File", command=upload_file)
+upload_button.pack(pady=20)
+
+txt_button = tk.Button(window,text="Go to text File!",command=go_to_text)
+txt_button.pack(pady = 30)
+
+def update_text(text):
+    text_box.insert(tk.END, text + "\n")
+    text_box.see(tk.END)
+
+text_box = tk.Text(window, width=50, height=70, font=("Arial", 12), wrap="word", borderwidth=2)
+text_box.pack(padx=15, pady=20)
+
+update_text("Please select the .RUN file you want to use!")
+
+window.mainloop()
 
 #Use any .run file you want
-
-begin("TestRun02.run")
